4934877d248495643946354320013cad4bb4622e
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2019-09-29 18:21:05 -0300

    Logica Movimiento, Creacion del Rocket

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        //We gonna ask wich key is pressed
+        if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
+        {
+            print("Thrusting");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+        
+       
+    }
+}
