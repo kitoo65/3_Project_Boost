f83da036c1a61bbab24269318cfe91a82263b8ae
kitoo65 <35207880+kitoo65@users.noreply.github.com>
2020-02-03 23:00:57 -0800

    Adding Sound

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,10 +7,13 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,21 +27,30 @@ public class Rocket : MonoBehaviour
         //We gonna ask wich key is pressed
         if (Input.GetKey(KeyCode.Space)) // Can thrust while rotating
         {
-            print("Thrusting");
-            rigidBody.AddRelativeForce(Vector3.up) ;
+            {
+                rigidBody.AddRelativeForce(Vector3.up);
+                print("Thrusting");
+                if (!audioSource.isPlaying) //Para que no se superponga 
+                {
+                    audioSource.Play();
+
+                }
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
             print("Rotating Left");
-            
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);
             print("Rotating Right");
         }
-        
-       
     }
 }
